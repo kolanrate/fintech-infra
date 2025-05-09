ebba674f9a6c1bc98a226ba1e45eebb79599c52e
Miracle Oluwole <miracle@Miracles-Laptop.lan>
2025-05-08 23:56:50 -0600

    s3 modification

--- prod/backend.tf
+++ prod/backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = "class38dominion-terraform-backend4"
-    region         = "us-east-1"
+    bucket         = "class38dominion-terraform-backend33"
+    region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
 }
